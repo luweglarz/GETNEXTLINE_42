d892869902d65287763fcaf1fac71926298551cc
Lucas WEGLARZ <lweglarz@e1r8p2.42.fr>
2020-08-10 12:49:09 +0200

    get_next_line.h first commit

--- get_next_line.h
+++ get_next_line.h
@@ -1,9 +1,25 @@
-# ifndef GET_NEXT_LINE_H
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lweglarz <lweglarz@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/08/10 12:34:54 by lweglarz          #+#    #+#             */
+/*   Updated: 2020/08/10 12:48:08 by lweglarz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
 # include <unistd.h>
 # include <stdlib.h>
 
 # define BUFFER_SIZE 32
+
+int		get_next_line(int fd, char **line);
+
+char	*ft_substr(char const *s, unsigned int start, size_t len);
 
 #endif
