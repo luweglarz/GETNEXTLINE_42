dcd1f3397d2f38ea16a5fb074bcd41f57501b6d7
Lucas WEGLARZ <lweglarz@e1r10p10.42.fr>
2020-08-13 14:43:17 +0200

    read a line with the size of the buffer

--- get_next_line.c
+++ get_next_line.c
@@ -1,6 +1,15 @@
 #include "get_next_line.h"
+#include <stdio.h>
 
 int get_next_line(int fd, char **line)
 {
+    char        buff[BUFFER_SIZE + 1];
+    int         ret;
+    int         pos;
 
+    pos = ft_strlenc(buff, '\n');
+    ret = read(fd, buff, BUFFER_SIZE);
+    *line = ft_substr(buff, 0, pos);
+
+    return (ret);
 }
